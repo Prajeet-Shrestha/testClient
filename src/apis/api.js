5f3a9e07a8d6580f9baf074e171723cdd085224c
Prajeet Shrestha <prajeets@ibriz.ai>
2025-04-29 12:52:22 +0545

    allForOne

---
+++ src/apis/api.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+const BASE_URL = "http://localhost:8080";
+export async function loginUser(email, password) {
+  try {
+    console.log("login user");
+    const response = await axios.post(BASE_URL + "/login", {
+      email,
+      password,
+    });
+    console.log({ response });
+    if (response.status == 200) {
+      return response.data;
+    } else {
+      throw new Error("User not found");
+    }
+  } catch (error) {
+    console.error(error);
+    throw new Error(error.response.data.message.toString());
+  }
+}
+export async function createUser(name, email, password, description) {
+  try {
+    console.log("login user");
+    const response = await axios.post(BASE_URL + "/createAccount", {
+      name,
+      email,
+      password,
+      description,
+    });
+    console.log({ response, d: response.data.message });
+    if (response.status == 200) {
+      return response.data;
+    } else {
+      throw new Error(response.data.message);
+    }
+  } catch (error) {
+    console.error(error);
+    throw new Error(error.response.data.message.toString());
+  }
+}
+export async function createPost(title, thumbnailUrl, subTitle, body, keywords, authorName, authorEmail, jwtToken) {
+  try {
+    console.log("createPost ==>");
+    const response = await axios.post(BASE_URL + "/createPost", {
+      title,
+      thumbnailUrl,
+      subTitle,
+      body,
+      keywords,
+      authorEmail,
+      authorName,
+      jwtToken,
+    });
+    console.log({ response, d: response.data.message });
+    if (response.status == 200) {
+      return response.data;
+    } else {
+      throw new Error(response.data.message);
+    }
+  } catch (error) {
+    console.error(error);
+    throw new Error(error.response.data.message.toString());
+  }
+}
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,110 @@
+import React, { useEffect, useState } from "react";
+import "./login.css";
+import error_outline from "../../assets/error_outline.svg";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import { useNavigate } from "react-router-dom";
+import { loginUser } from "../../apis/api";
+import Spinner from "react-bootstrap/Spinner";
+
+function Login({ setisLoggedIn }) {
+  const nav = useNavigate();
+  const [email, setemail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorName, seterrorName] = useState("");
+  const [isLoading, setisLoading] = useState(false);
+  useEffect(() => {
+    // const res = loginUser();
+
+    return () => {};
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <div className='mindBridgeMainContainer'>
+        <div className='loginWithPW'>
+          <div className='signInFormControl'>
+            <div className='title'>
+              <h3>Login to your account</h3>
+            </div>
+            <form className='form'>
+              <div className='formInputBox'>
+                <div className='ForumEmail'>
+                  <input
+                    type='email'
+                    value={email}
+                    onChange={(e) => {
+                      setemail(e.target.value);
+                    }}
+                    placeholder='Your email address'
+                  />
+                </div>
+              </div>
+              <div className='formInputBox'>
+                <div className='ForumEmail'>
+                  <input
+                    type='password'
+                    value={password}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
+                    placeholder='Your password'
+                  />
+                </div>
+              </div>
+              {errorName.length >= 1 && (
+                <div className='invalidEmailErrorSection'>
+                  <img className='invalidEmailErrorSVG' src={error_outline} />
+                  <p>{errorName}</p>
+                </div>
+              )}
+              <div className='buttonFlex'>
+                <button
+                  className='loginBtn'
+                  type='button'
+                  disabled={isLoading || email.length <= 0 || password.length <= 0}
+                  onClick={async (e) => {
+                    seterrorName("");
+                    setisLoading(true);
+                    try {
+                      e.preventDefault(); // Prevent form submission
+                      console.log({ email, password });
+                      const res = await loginUser(email, password);
+                      setisLoggedIn(true);
+                      localStorage.removeItem("user");
+                      localStorage.setItem("user", JSON.stringify(res.result.jwtToken));
+                      nav("/dashboard");
+                      console.log(res);
+                    } catch (error) {
+                      seterrorName(error.toString());
+                    } finally {
+                      setisLoading(false);
+                    }
+                  }}>
+                  {isLoading ? "Verifying.." : "Login"}
+                </button>
+                {isLoading && <Spinner animation='border' size='sm' className='spinner' variant='dark' />}
+              </div>
+            </form>
+          </div>
+          <div className='SignUpPageRedirect'>
+            <p>
+              Looking to create an account?{" "}
+              <span
+                className='blue btn'
+                onClick={() => {
+                  nav("/signUp");
+                }}>
+                Click here
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default Login;
